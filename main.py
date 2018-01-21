a7012e32d0834520e518d2f714cce76d83eb8708
IHasCancer <robertha290@nemi.tskoli.is>
2018-01-21 17:41:05 +0000

    X button working

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('Yall can go suck a catapillar dick')
-print("skrrrt")
+import pygame
+
+pygame.init()
+
+window_size = window_width, widow_height = 1280, 720
+window = pygame.display.set_mode(window_size, pygame.RESIZABLE)
+
+pygame.display.set_caption('Skilaverkefni01')
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
