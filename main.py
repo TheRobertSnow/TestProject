1cedb8d9d684473d0845c29782332cbc4cd07048
IHasCancer <robertha290@nemi.tskoli.is>
2018-01-21 21:06:32 +0000

    def test on game_intro

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ window = pygame.display.set_mode(window_size, pygame.RESIZABLE)
 
 pygame.display.set_caption('Skilaverkefni01')
 
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
 
 
 pygame.quit()
